6c1a8cbb00c1984ded538a7ffa27ec2b1b39835d
DanielReisdC <49843024+DanielReisdC@users.noreply.github.com>
2023-01-15 14:39:31 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,220 +1,240 @@
-import java.time.LocalDate;
-import java.util.ArrayList;
+
+import java.util.List;
 import java.util.Scanner;
-public class App {
-    static ArrayList<Usuario> usuario;
-    static ArrayList<Imovel> imovel;
-    static int tipo;
+import java.util.ArrayList;
+public class App{
 
-    public static void main(String[] args) {
-        usuario = new ArrayList<>();
-        imovel =new ArrayList<>();
-
-        int comandoInteiro = 0;
-        while (comandoInteiro != 5){
-            opcoesServicos();
-        }
+public static void main(String[] args) {
+    Scanner scanner= new Scanner(System.in);
+    ArrayList<Usuario>usuarios=new ArrayList();
+    ArrayList<Imovel>imoveis = new ArrayList();
+    ArrayList<Registro>registro = new ArrayList();
+    
+    boolean rodando = true;
+    while (rodando){
+    	System.out.println("[1]Cadastrar um imóvel ");
+    	System.out.println("[2]Cadastrar um usuário ");
+    	System.out.print("Digite a opção: ");
+    	String operacao=scanner.nextLine();
+    	 switch(operacao){
+ 	    case "1":{
+ 	    	 while(rodando){
+ 	    		System.out.println("\n==== Bem-vindo ao cadastro de imóveis ==== ");
+ 	       	 System.out.println("\n[1]Cadastrar imóvel ");
+ 	       	    System.out.println("[2]Listar todos os imóvel");
+ 	       	    System.out.println("[3]Atualizar dados de um imóvel ");
+ 	       	    System.out.println("[4]Remover imóvel ");
+ 	       	    System.out.println("[5]Sair");
+ 	       	    System.out.print("Digite a opção: ");
+ 	       	    String opcao=scanner.nextLine();
+ 	       	    switch(opcao){
+ 	       	    case "1":{
+ 	       	    	System.out.println("==== CADASTRO DE IMÓVEL ==== ");
+ 	       	    	System.out.println(" Digite a descricao: ");
+ 	       	    	String descricao = scanner.nextLine();
+ 	       	    	System.out.println(" Digite o valor do aluguel: ");
+ 	       	    	String valor_aluguel = scanner.nextLine();
+ 	       	    	System.out.println(" Digite o endereco completo: ");
+ 	       	    	String endereco_completo = scanner.nextLine();
+ 	       	    	System.out.println(" Digite a data de cadastro: ");
+ 	       	    	String data_cadastro=scanner.nextLine();
+ 	       	    	Imovel im = new Imovel(data_cadastro, data_cadastro, data_cadastro, null);
+ 	       	    	im.setDescricao(descricao);
+ 	       	    	im.setValor_aluguel(valor_aluguel);
+ 	       	    	im.setEndereco_completo(endereco_completo);
+ 	       	    	im.setData_cadastro(data_cadastro);
+ 	       	    	
+ 	       	    	imoveis.add(im);
+ 	       	    	
+ 	       	    	
+ 	       	 
+ 	       	        break;
+ 	       	    	
+ 	       	    }
+ 	       	        
+ 	       	    case "2":{
+ 	       	    	System.out.println("==== IMÓVEIS CADASTRADOS ==== ");
+ 	       	    	for(int i=0;i<imoveis.size();i++){
+ 	       	    		
+ 	       	    		Imovel uTemp = imoveis.get(i);
+ 	       	    		System.out.println(" imóvel " +i);
+ 	       	    		System.out.println("\tdescrição: " + imoveis.get(i).getDescricao());
+ 	       	    		System.out.println("\tvalor do aluguel " + imoveis.get(i).getValor_aluguel());
+ 	       	    		System.out.println("\tEndereço completo: " + imoveis.get(i).getEndereco_completo());
+ 	       	    		System.out.println("\tData de cadastro " + imoveis.get(i).getData_cadastro());
+ 	       	    		
+ 	       	    		
+ 	       	    	}
+ 	       	        break;
+ 	       	    	
+ 	       	    }
+ 	       	        
+ 	       	    case "3":{
+ 	       	    	System.out.println("==== ATUALIZAÇÃO DE IMÓVEL ==== ");
+ 	       	    	for(int i=0;i<imoveis.size();i++){
+ 	       	    		Imovel uTemp=imoveis.get(i);
+ 	       	    		System.out.println("[" + i +"]" + uTemp.getDescricao());
+ 	       	    		
+ 	       	    	}
+ 	       	    	System.out.println(" Digite um valor de referência: ");
+ 	       	    	int referencia=scanner.nextInt();
+ 	       	    	scanner.nextLine();
+ 	       	    	System.out.println(" Digite o novo nome: ");
+ 	       	    	String novoDescricao = scanner.nextLine();
+ 	       	    	System.out.println(" Digite o novo email: ");
+ 	       	    	String novoValor_aluguel = scanner.nextLine();
+ 	       	    	System.out.println(" Digite o nova data de nascimento: ");
+ 	       	    	String novaEndereco_completo = scanner.nextLine();
+ 	       	    	System.out.println(" Digite o novo Endereço: ");
+ 	       	    	String novoData_cadastro = scanner.nextLine();
+ 	       	    	Imovel im = imoveis.get(referencia);
+ 	       	    	im.setDescricao(novoDescricao);
+ 	       	    	im.setValor_aluguel(novoData_cadastro);
+ 	       	    	im.setEndereco_completo(novaEndereco_completo);
+ 	       	    	im.setData_cadastro(novoData_cadastro);
+ 	       	    	
+ 	       	    	
+ 	       	    	
+ 	       	         break;
+ 	       	    }
+ 	       	       
+ 	       	    case "4":{
+ 	       	    	System.out.println("==== REMOVER USUÁRIO==== ");
+ 	       	    	
+ 	       	    	for(int i=0;i<imoveis.size();i++){
+ 	       	    		Imovel uTemp=imoveis.get(i);
+ 	       	    		System.out.println("[" + i +"]" + uTemp.getDescricao());
+ 	       	    	}
+ 	       	    		System.out.println(" Digite um valor de referência: ");
+ 	           	    	int referencia=scanner.nextInt();
+ 	           	    	scanner.nextLine();
+ 	           	    	imoveis.remove(referencia);
+ 	       	         break;
+ 	       	    }
+ 	       	    
+ 	       	       
+ 	       	    case "5":{
+ 	       	    	rodando=false;
+ 	       	    	
+ 	       	         break;
+ 	       	    }
+ 	       	    }
+ 	    	 }
+ 	    }
+ 	        
+ 	    case "2":{
+ 	    	while(rodando){
+ 	    		System.out.println("\n==== Bem-vindo ao cadastro de usuários ==== ");
+ 	      	 System.out.println("\n[1]Cadastrar usuário ");
+ 	      	    System.out.println("[2]Listar todos os usuário");
+ 	      	    System.out.println("[3]atualizar dados de um usuário ");
+ 	      	    System.out.println("[4]Remover usuário ");
+ 	      	    System.out.println("[5]Sair");
+ 	      	    System.out.print("Digite a opção: ");
+ 	      	    String opcao=scanner.nextLine();
+ 	      	    switch(opcao){
+ 	      	    case "1":{
+ 	      	    	System.out.println("==== CADASTRO DE USUÁRIO ==== ");
+ 	      	    	System.out.println(" Digite o nome: ");
+ 	      	    	String nome = scanner.nextLine();
+ 	      	    	System.out.println(" Digite o email: ");
+ 	      	    	String email = scanner.nextLine();
+ 	      	    	System.out.println(" Digite a data de nascimento: ");
+ 	      	    	String data_nasc = scanner.nextLine();
+ 	      	    	System.out.println(" Digite o endereço completo: ");
+ 	      	    	String endereco_completo= scanner.nextLine();
+ 	      	    	Usuario u = new Usuario(email, email, null, email);
+ 	      	    	u.setNome(nome);
+ 	      	    	u.setEmail(email);
+ 	      	    	u.setData_nasc(data_nasc);
+ 	      	    	u.setEndereco_completo(endereco_completo);
+ 	      	    	
+ 	      	    	usuarios.add(u);
+ 	      	    	
+ 	      	    	
+ 	      	 
+ 	      	        break;
+ 	      	    	
+ 	      	    }
+ 	      	        
+ 	      	    case "2":{
+ 	      	    	System.out.println("==== USUÁRIOS CADASTRADOS ==== ");
+ 	      	    	for(int i=0;i<usuarios.size();i++){
+ 	      	    		
+ 	      	    		Usuario uTemp = usuarios.get(i);
+ 	      	    		System.out.println(" Usuário " +i);
+ 	      	    		System.out.println("\tNome: " + usuarios.get(i).getNome());
+ 	      	    		System.out.println("\tEmail: " + usuarios.get(i).getEmail());
+ 	      	    		System.out.println("\tData de Nascimento: " + usuarios.get(i).getData_nasc());
+ 	      	    		System.out.println("\tEndereço completo: " + usuarios.get(i).getEndereco_completo());
+ 	      	    		
+ 	      	    		
+ 	      	    	}
+ 	      	        break;
+ 	      	    	
+ 	      	    }
+ 	      	        
+ 	      	    case "3":{
+ 	      	    	System.out.println("==== ATUALIZAÇÃO DE USUÁRIO ==== ");
+ 	      	    	for(int i=0;i<usuarios.size();i++){
+ 	      	    		Usuario uTemp=usuarios.get(i);
+ 	      	    		System.out.println("[" + i +"]" + uTemp.getNome());
+ 	      	    		
+ 	      	    	}
+ 	      	    	System.out.println(" Digite um valor de referência: ");
+ 	      	    	int referencia=scanner.nextInt();
+ 	      	    	scanner.nextLine();
+ 	      	    	System.out.println(" Digite o novo nome: ");
+ 	      	    	String novoNome = scanner.nextLine();
+ 	      	    	System.out.println(" Digite o novo email: ");
+ 	      	    	String novoEmail = scanner.nextLine();
+ 	      	    	System.out.println(" Digite o nova data de nascimento: ");
+ 	      	    	String novaData_nasc = scanner.nextLine();
+ 	      	    	System.out.println(" Digite o novo Endereço: ");
+ 	      	    	String novoEndereco = scanner.nextLine();
+ 	      	    	Usuario u = usuarios.get(referencia);
+ 	      	    	u.setNome(novoNome);
+ 	      	    	u.setEmail(novoEmail);
+ 	      	    	u.setData_nasc(novaData_nasc);
+ 	      	    	u.setEndereco_completo(novoEndereco);
+ 	      	    	
+ 	      	    	
+ 	      	    	
+ 	      	         break;
+ 	      	    }
+ 	      	       
+ 	      	    case "4":{
+ 	      	    	System.out.println("==== REMOVER USUÁRIO==== ");
+ 	      	    	
+ 	      	    	for(int i=0;i<usuarios.size();i++){
+ 	      	    		Usuario uTemp=usuarios.get(i);
+ 	      	    		System.out.println("[" + i +"]" + uTemp.getNome());
+ 	      	    	}
+ 	      	    		System.out.println(" Digite um valor de referência: ");
+ 	          	    	int referencia=scanner.nextInt();
+ 	          	    	scanner.nextLine();
+ 	          	    	usuarios.remove(referencia);
+ 	      	         break;
+ 	      	    }
+ 	      	    
+ 	      	       
+ 	      	    case "5":{
+ 	      	    	rodando=false;
+ 	      	    	
+ 	      	         break;
+ 	      	    }
+    	
     }
-    public static void opcoesServicos(){
-        System.out.println("SERVIÇOS:\n1-Cadastrar\n2-Alterar\n3-Listar\n4-Remover\n5-Sair");
-        int comando;
-        System.out.println("Digite sua opção");
-        Scanner scanner = new Scanner(System.in);
-        comando = scanner.nextInt();
-        switch (comando) {
-            case 1:
-                cadastrar();
-                break;
-            case 2:
-                alterar();
-                break;
-            case 3:
-                listar();
-                break;
-            case 4:
-                remover();
-                break;
-            case 5:
-                sair();
-                break;
-            
-            default:
-        
-
-        }
-
+    
+    	
+    	
     }
-    private static void cadastrar(){
-        String[] options = {"Cadastrar Serviço", "Cadastrar Cliente"};
-        int tipoCadastro = showOptionDialog(null, "Escolha o tipo de cadastro:",
-                "Clique em uma opção",
-                DEFAULT_OPTION, INFORMATION_MESSAGE, null, options, options[0]);
-        if(tipoCadastro == 0) {
-            tipo = 0;
-            cadastrarServico();
-        }
-        else if (tipoCadastro == 1) {
-            tipo = 1;
-            cadastrarCliente();
-        }
+   
+       
+    
     }
-    private static void cadastrarServico(){
-           String nome = JOptionPane.showInputDialog("Qual o nome do serviço realizado: ");
-           String descricao = JOptionPane.showInputDialog("Descreva o serviço realizado: ");
-           String valorString = JOptionPane.showInputDialog("Qual o valor do serviço realizado: ");
-           double valor = Double.parseDouble(valorString);
-           String funcionario = JOptionPane.showInputDialog("Nome do funcionário que realizou o serviço: ");
-           Funcionario Funcionarioo = new Funcionario(funcionario);
-           Servicos servico = new Servicos(nome, descricao, valor,Funcionarioo);
-           servicos.add(servico);
-    }
-    private static Servicos encontraServicos(int codigo){
-        Servicos servico = null;
-        if(servicos.size() > 0){
-            for(Servicos s :servicos){
-                if(s.getCodigo() == codigo){
-                    servico = s;
-                }
-            }
-        }
-
-        return servico;
-    }
-    private static void cadastrarCliente(){
-        String nome = JOptionPane.showInputDialog("Digite o nome do cliente: ");
-        String email = JOptionPane.showInputDialog("Digite o email do cliente: ");
-        String sexo = JOptionPane.showInputDialog("Digite o sexo do cliente: ");
-
-        String uf = JOptionPane.showInputDialog("Digite a UF do cliente: ");
-        String cidade = JOptionPane.showInputDialog("Digite o nome da cidade do cliente: ");
-        String bairro = JOptionPane.showInputDialog("Digite o nome do bairro do cliente: ");
-        String logradouro = JOptionPane.showInputDialog("Digite o logradouro do cliente: ");
-        String numero = JOptionPane.showInputDialog("Digite o número da casa do cliente: ");
-        Endereco endereco = new Endereco(logradouro, numero, bairro, cidade, uf);
-        Cliente cliente = new Cliente(nome, email, sexo, endereco);
-        clientes.add(cliente);
-        JOptionPane.showMessageDialog(null,"Cliente cadastrado com sucesso!");
-    }
-    private static Cliente encontrarClientes(int codigo){
-        Cliente cliente = null;
-        if(clientes.size() > 0){
-            for(Cliente c : clientes){
-                if(c.getCodigo() == codigo){
-                    cliente = c;
-                }
-            }
-        }
-        return cliente;
-    }
-    private static void alterar(){
-        String[] options = {"Alterar dados do Serviço", "Alterar dados do Cliente"};
-        int opcao = showOptionDialog(null, "Escolha uma opção:",
-                "Alterar",
-                DEFAULT_OPTION, INFORMATION_MESSAGE, null, options, options[0]);
-        if(opcao == 0) {
-            alterarServicos();
-        }
-        else if (opcao == 1) {
-            alterarCliente();
-        }
-    }
-    public static void alterarServicos() {
-        String codigoString = JOptionPane.showInputDialog("Digite o código do serviço a ser alterado :  ");
-        int codigo = Integer.parseInt(codigoString);
-        Servicos servico = encontraServicos(codigo);
-        if( servico != null){
-            String nome = JOptionPane.showInputDialog("Qual o nome do serviço realizado: ");
-            String descricao = JOptionPane.showInputDialog("Descreva o serviço realizado: ");
-            String valorString = JOptionPane.showInputDialog("Qual o valor do serviço realizado: ");
-            double valor = Double.parseDouble(valorString);
-            String nomefuncionario = JOptionPane.showInputDialog("Nome do funcionário que realizou o serviço: ");
-            servico.alterar(servico, codigo, nome, valor, descricao, nomefuncionario);
-            JOptionPane.showMessageDialog(null, " Serviço alterado com sucesso");
-        }
-    }
-    public static void alterarCliente() {
-        String codigoString = JOptionPane.showInputDialog("Digite o código do cliente a ser alterado :  ");
-        int codigo = Integer.parseInt(codigoString);
-        Cliente cliente =encontrarClientes(codigo);
-        if( cliente != null){
-            String nome = JOptionPane.showInputDialog("Digite o nome do cliente: ");
-            String email = JOptionPane.showInputDialog("Digite o email do cliente: ");
-            String sexo = JOptionPane.showInputDialog("Digite o sexo do cliente: ");
-
-            String uf = JOptionPane.showInputDialog("Digite a UF do cliente: ");
-            String cidade = JOptionPane.showInputDialog("Digite o nome da cidade do cliente: ");
-            String bairro = JOptionPane.showInputDialog("Digite o nome do bairro do cliente: ");
-            String logradouro = JOptionPane.showInputDialog("Digite o logradouro do cliente: ");
-            String numero = JOptionPane.showInputDialog("Digite o número da casa do cliente: ");
-            cliente.alterarCliente(cliente, codigo, nome, email, sexo, uf, cidade, bairro, logradouro, numero);
-            JOptionPane.showMessageDialog(null, "Cliente alterado com sucesso!");
-        }
-    }
-    private static void listar(){
-        String[] options = {"Listar serviços", "Listar clientes"};
-        int opcao = showOptionDialog(null, "Escolha uma opção:",
-                "Listar",
-                DEFAULT_OPTION, INFORMATION_MESSAGE, null, options, options[0]);
-        if(opcao == 0) {
-            listarServicos();
-        }
-        else if (opcao == 1) {
-            listarClientes();
-        }
-    }
-    private static void listarServicos() {
-        if (servicos.size() > 0) {
-            for (Servicos servico : servicos) {
-                JOptionPane.showMessageDialog(null,""+servico);
-            }
-        }
-        else {
-            JOptionPane.showMessageDialog(null, "Não há serviços para listar!");
-        }
-    }
-    private static void listarClientes() {
-        if (clientes.size() > 0) {
-            for (Cliente cliente : clientes) {
-                JOptionPane.showMessageDialog(null,""+cliente);
-            }
-        }
-        else {
-            JOptionPane.showMessageDialog(null, "Não há clientes para listar!");
-        }
-    }
-    private static void remover(){
-        String[] options = {"Remover serviço", "Remover cliente"};
-        int opcao = showOptionDialog(null, "Escolha uma opção:",
-                "Remover",
-                DEFAULT_OPTION, INFORMATION_MESSAGE, null, options, options[0]);
-        if(opcao == 0) {
-            removerServico();
-            JOptionPane.showMessageDialog(null, "Remoção realizada com sucesso!");
-        }
-        else if (opcao == 1) {
-            removerCliente();
-            JOptionPane.showMessageDialog(null, "Remoção realizada com sucesso!");
-        }
-    }
-    public static void removerServico() {
-        String codigoString = JOptionPane.showInputDialog("Digite o código do serviço a ser remolvido: ");
-        int codigo = Integer.parseInt(codigoString);
-        Servicos servico = encontraServicos(codigo);
-        if(servico != null) {
-            servico.remover(servico, codigo);
-        }
-    }
-    public static void removerCliente() {
-        String codigoString = JOptionPane.showInputDialog("Digite o código do cliente a ser remolvido: ");
-        int codigo = Integer.parseInt(codigoString);
-        Cliente cliente = encontrarClientes(codigo);
-        if(cliente != null){
-            cliente.removerClString comando = Systemientes(cliente, codigo);
-        }
-    }
-    private static void sair(){
-        showMessageDialog(null, "Saída executada!");
-        System.exit(0);
-    }
-
+    
+}
+}
+}
 }
