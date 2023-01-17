65615a2b64954901f2a60fe1dd4d3086fa0d4957
Renanlucass <106043243+Renanlucass@users.noreply.github.com>
2023-01-17 20:05:59 -0300

    Create Endereço.java

---
+++ src/Endereço.java
@@ -0,0 +1,51 @@
+public class Endereco {
+
+    private String rua;
+    private String bairro;
+    private int numero;
+    private String cidade;
+    private String uf;
+    
+
+    
+    public Endereco(String rua, String bairro, int numero, String cidade, String uf) {
+        this.rua = rua;
+        this.bairro = bairro;
+        this.numero = numero;
+        this.cidade = cidade;
+        this.uf = uf;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getUf() {
+        return uf;
+    }
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    
+}
